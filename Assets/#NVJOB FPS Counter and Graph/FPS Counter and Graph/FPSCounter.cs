cb650d95a0bcc5d90d06c731ac10ef5646b1fd47
#NVJOB Nicholas Veselov <reset86@gmail.com>
2020-03-08 17:08:09 +0300

    v2.0

--- Assets/#NVJOB FPS Counter and Graph/FPS Counter and Graph/FPSCounter.cs
+++ Assets/#NVJOB FPS Counter and Graph/FPS Counter and Graph/FPSCounter.cs
@@ -58,6 +58,7 @@ public class FPSCounter : MonoBehaviour
     {
         //--------------
 
+        Application.targetFrameRate = highestPossibleFPS;
         CreateCounter();
 
         //--------------
